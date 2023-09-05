2851cb55c14254d8fb4498018d09b0634a5b0053
jitujiten <jsahu5425@gmail.com>
2023-09-05 11:06:59 +0530

    "adding some styling"

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,84 +2,92 @@
   <div class="p-3 bg-black relative">
     <section class="webdesigntuts-workshop py-3">
       <form @submit.prevent="submitForm">
-        <div class="p-2">
-          <label for="name" class="waviy"
-            ><span style="--i: 1">N</span>
-            <span style="--i: 2">A</span>
-            <span style="--i: 3">M</span>
-            <span style="--i: 4">E</span>
-            <span style="--i: 4">:</span>
-          </label>
+        <div>
+          <label for="name">
+            <h2 class="title text-2xl md:text-5xl">
+              <span class="title-word title-word-1">N</span>
+              <span class="title-word title-word-2">A</span>
+              <span class="title-word title-word-3">M</span>
+              <span class="title-word title-word-4">E</span>
+              <span class="title-word title-word-1">:</span>
+            </h2></label
+          >
           <input
             v-model="formData.name"
             type="text"
             name="name"
             id="name"
-            class="w-4/5 md:w-3/5 p-2"
+            class="w-4/5 md:w-3/5 p-2 ml-4"
           />
           <br />
-          <span v-if="!isNameValid" class="text-sm md:text-xl text-red-600"
+          <span
+            v-if="!isNameValid"
+            class="text-sm md:text-xl text-red-600 ml-5 mt-1"
             >Name is required.</span
           >
         </div>
 
         <div class="p-2">
-          <label for="contactNumber" class="waviy"
-            ><span style="--i: 1">C</span>
-            <span style="--i: 2">O</span>
-            <span style="--i: 3">N</span>
-            <span style="--i: 4">T</span>
-            <span style="--i: 5">A</span>
-            <span style="--i: 6">C</span>
-            <span style="--i: 7">T</span>
-            <span style="--i: 7"> </span>
-            <span style="--i: 8">N</span>
-            <span style="--i: 9">U</span>
-            <span style="--i: 10">M</span>
-            <span style="--i: 11">B</span>
-            <span style="--i: 12">E</span>
-            <span style="--i: 13">R</span>
-            <span style="--i: 13">:</span>
+          <label for="contactNumber">
+            <h2 class="title text-2xl md:text-5xl">
+              <span class="title-word title-word-1">C</span>
+              <span class="title-word title-word-2">O</span>
+              <span class="title-word title-word-3">N</span>
+              <span class="title-word title-word-4">T</span>
+              <span class="title-word title-word-1">A</span>
+              <span class="title-word title-word-1">C</span>
+              <span class="title-word title-word-2">T</span>
+              <span class="title-word title-word-3">&nbsp;</span>
+              <span class="title-word title-word-4">N</span>
+              <span class="title-word title-word-1">U</span>
+              <span class="title-word title-word-1">M</span>
+              <span class="title-word title-word-2">B</span>
+              <span class="title-word title-word-3">E</span>
+              <span class="title-word title-word-4">R</span>
+              <span class="title-word title-word-1">:</span>
+            </h2>
           </label>
           <input
             v-model="formData.contactNumber"
             type="text"
             name="contactNumber"
             id="contactNumber"
-            class="w-4/5 md:w-3/5 p-2"
+            class="w-4/5 md:w-3/5 p-2 ml-4"
           />
           <br />
           <span
             v-if="!isContactNumberValid"
-            class="text-sm md:text-xl text-red-600"
+            class="text-sm md:text-xl text-red-600 ml-2 mt-1"
             >Please enter a valid 10-digit contact number.</span
           >
         </div>
 
         <div class="p-2">
-          <label for="aboutWork" class="waviy">
-            <span style="--i: 1">A</span>
-            <span style="--i: 2">B</span>
-            <span style="--i: 3">O</span>
-            <span style="--i: 4">U</span>
-            <span style="--i: 5">T</span>
-            <span style="--i: 5"> </span>
-            <span style="--i: 6">W</span>
-            <span style="--i: 7">O </span>
-            <span style="--i: 8">R</span>
-            <span style="--i: 9">K</span>
-            <span style="--i: 9">:</span>
+          <label for="aboutWork">
+            <h2 class="title text-2xl md:text-5xl">
+              <span class="title-word title-word-1">A</span>
+              <span class="title-word title-word-2">B</span>
+              <span class="title-word title-word-3">O</span>
+              <span class="title-word title-word-4">U</span>
+              <span class="title-word title-word-1">T</span>
+              <span class="title-word title-word-1">&nbsp;</span>
+              <span class="title-word title-word-2">W</span>
+              <span class="title-word title-word-3">O</span>
+              <span class="title-word title-word-4">R</span>
+              <span class="title-word title-word-1">K</span>
+              <span class="title-word title-word-1">:</span>
+            </h2>
           </label>
           <textarea
             v-model="formData.aboutWork"
             name="aboutWork"
             id="aboutWork"
-            class="w-4/5 md:w-3/5 p-2"
+            class="w-4/5 md:w-3/5 p-2 ml-4"
           ></textarea>
         </div>
 
         <button
-          class="glow-on-hover p-4 mt-4 ml-1 text-md text-center"
+          class="glow-on-hover p-4 mt-4 ml-6 text-md text-center"
           type="submit"
         >
           Submit
@@ -90,43 +98,6 @@
   </div>
 </template>
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap");
-
-.waviy {
-  position: relative;
-  -webkit-box-reflect: below -20px linear-gradient(transparent, rgba(0, 0, 0, 0.2));
-  font-size: 45px;
-}
-
-.waviy span {
-  font-family: "Alfa Slab One", cursive;
-  position: relative;
-  display: inline-block;
-  color: #fff;
-  text-transform: uppercase;
-  animation: waviy 1s infinite;
-  animation-delay: calc(0.1s * var(--i));
-}
-
-@media (max-width: 768px) {
-  .waviy {
-    -webkit-box-reflect: below -10px linear-gradient(transparent, transparent);
-    font-size: 25px;
-  }
-}
-@keyframes waviy {
-  0%,
-  40%,
-  100% {
-    transform: translateY(0);
-  }
-  20% {
-    transform: translateY(-20px);
-  }
-}
-.containers {
-  box-shadow: 1px 2px 60px 4px #ff004f;
-}
 label {
   display: block;
   margin: 20px;
@@ -156,6 +127,65 @@ textarea:focus {
   border-color: #26bf47;
   box-shadow: 0 0 10px #26bf47;
 }
+
+.title-word {
+  animation: color-animation 4s linear infinite;
+}
+
+.title-word-1 {
+  --color-1: #df8453;
+  --color-2: #3d8dae;
+  --color-3: #e4a9a8;
+}
+
+.title-word-2 {
+  --color-1: #dbad4a;
+  --color-2: #accfcb;
+  --color-3: #17494d;
+}
+
+.title-word-3 {
+  --color-1: #accfcb;
+  --color-2: #e4a9a8;
+  --color-3: #accfcb;
+}
+
+.title-word-4 {
+  --color-1: #3d8dae;
+  --color-2: #df8453;
+  --color-3: #e4a9a8;
+}
+
+@keyframes color-animation {
+  0% {
+    color: var(--color-1);
+  }
+  32% {
+    color: var(--color-1);
+  }
+  33% {
+    color: var(--color-2);
+  }
+  65% {
+    color: var(--color-2);
+  }
+  66% {
+    color: var(--color-3);
+  }
+  99% {
+    color: var(--color-3);
+  }
+  100% {
+    color: var(--color-1);
+  }
+}
+
+.title {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 800;
+  text-transform: uppercase;
+}
+
 .glow-on-hover {
   width: 220px;
   height: 50px;
@@ -234,8 +264,6 @@ textarea:focus {
 }
 </style>
 <script>
-import axios from "axios";
-
 import Toast from "../components/Toast.vue";
 export default {
   data() {
@@ -266,26 +294,34 @@ export default {
           aboutWork: this.formData.aboutWork,
         };
 
-        try {
-          const response = await axios.post(
-            "https://sheetdb.io/api/v1/itwwf1ylpy76v",
-            postData
-          );
-          if (response.status === 201) {
-            this.ToastVisible = true;
-            setTimeout(() => {
-              this.ToastVisible = false;
-            }, 2000);
-            console.log("Form submitted successfully:", this.formData);
-          } else {
-            console.error(
-              "Form submission failed. API returned:",
-              response.data
-            );
-          }
-        } catch (error) {
-          console.error("Error submitting form:", error);
-        }
+        fetch("https://sheetdb.io/api/v1/itwwf1ylpy76v", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(postData),
+        })
+          .then((response) => {
+            if (response.status === 201) {
+              this.ToastVisible = true;
+              setTimeout(() => {
+                this.ToastVisible = false;
+              }, 2000);
+              console.log("Form submitted successfully:", this.formData);
+              this.formData = {
+                name: "",
+                contactNumber: "",
+                aboutWork: "",
+              };
+            } else {
+              return response.json().then((data) => {
+                console.error("Form submission failed. API returned:", data);
+              });
+            }
+          })
+          .catch((error) => {
+            console.error("Error submitting form:", error);
+          });
       }
     },
   },
